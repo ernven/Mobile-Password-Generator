d503de2baf31e1e045a1a6a24cfe8685b7ff6d75
Ernesto V R <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2020-04-19 20:15:10 +0300

    React Native project created, basic screens added

---
+++ UserDetails.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+export default function UserDetails(props) {
+    const [user, setUser] = useState(props.user);
+
+    return (
+        <View>
+
+        </View>
+    );
+}
