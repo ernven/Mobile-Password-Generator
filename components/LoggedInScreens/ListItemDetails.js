aa8d52ecbd83278dc6b542478ca643f2a4f50e95
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-04-09 21:31:05 +0300

    Small layout fix

--- components/LoggedInScreens/ListItemDetails.js
+++ components/LoggedInScreens/ListItemDetails.js
@@ -41,7 +41,7 @@ export default function ListItemDetails({ item, deleteItem }) {
       >
         <View style={styles.container}>
           <Header
-            containerStyle={{ marginTop: '25%', backgroundColor: '#0065ca' }}
+            containerStyle={{ marginTop: '15%', backgroundColor: '#0065ca' }}
             centerComponent={{ text: item.name, style: styles.headerText }}
           />
           <View style={styles.body}>
@@ -50,7 +50,7 @@ export default function ListItemDetails({ item, deleteItem }) {
             <Text h4 >Password</Text>
             <Text h4 style={styles.bodyText} selectable={true}>{item.password}</Text>
           </View>
-          <View style={{ flex: 1.5 }}>
+          <View>
             <Button
               buttonStyle={styles.deleteButton}
               icon={<Icon name="md-trash" size={20} style={{ paddingRight: 10 }} color="#ffffff" />}
@@ -69,14 +69,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5
   },
   overlay: {
-    marginTop: 32,
-    marginBottom: 45,
-    marginHorizontal: 35,
+    height: '90%',
+    marginHorizontal: '8%',
     padding: 0,
     borderRadius: 20
   },
   container: {
     alignItems: 'center',
+    paddingBottom: '12%'
   },
   headerText: {
     color: '#ffffff',
@@ -84,13 +84,12 @@ const styles = StyleSheet.create({
     paddingVertical: '25%'
   },
   body: {
-    paddingVertical: '5%',
-    paddingTop: '30%',
+    paddingVertical: '20%',
     alignItems: 'center'
   },
   bodyText: {
     color: 'grey',
-    marginBottom: '25%'
+    marginBottom: '12%'
   },
   deleteButton: {
     backgroundColor: '#d43131',
